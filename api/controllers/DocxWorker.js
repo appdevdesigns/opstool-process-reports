2eff877e42291eaa3eaced5d82787ee9f95fca31
Joshua Chan <joshua@appdevdesigns.net>
2019-01-24 02:05:03 -0800

    Revert "Reuse the same child process for all render jobs"
    This reverts commit 80c4a28716b4860dca9b5b101be75517235fa203.

--- api/controllers/DocxWorker.js
+++ api/controllers/DocxWorker.js
@@ -5,82 +5,31 @@
  * with the parent process through IPC messages.
  */
 
-// process.send() should only be available if this file was executed 
-// with child_process.fork(). If not, then this file was added with require().
+var DocxGen = require('docxtemplater');
+var DocxImageModule = require('docxtemplater-image-module');
+var sizeOf = require('image-size');
+var fs = require('fs');
+var path = require('path');
+var async = require('async');
 
-// For require()
-if (!process.send) {
-	var child_process = require('child_process');
-	
-	// Start the Docx worker child process. This will be reused for every 
-	// rendering job.
-	var workerProcess = child_process.fork(__dirname + '/DocxWorker.js');
-	if (!workerProcess) {
-		console.error('Unable to launch DocxWorker.js');
-	}
-	var workerJobCount = 0;
-	process.on('exit', () => {
-		workerProcess && workerProcess.kill();
-	});
-	
+// process.send() should only be available if this file was executed with child_process.fork()
+if (process.send) {
 	/**
-	 * Perform Docx rendering in a separate thread.
-	 *
-	 * @param {object} options
-	 * @param {string} options.templateFile
+	 * @param {object} msg
+	 * @param {string} msg.templateFile
 	 *		The full path of the Docx template file.
-	 * @param {object} options.data
+	 * @param {object} msg.data
 	 *		The data to be used for populating the template.
 	 *
-	 * @param {object} [options.image]
-	 * @param {string} [options.image.path]
+	 * @param {object} [msg.image]
+	 * @param {string} [msg.image.path]
 	 *		The full path to the image files.
-	 * @param {integer} [options.image.maxWidth]
-	 * @param {integer} [options.image.maxHeight]
-	 * @param {array} [options.image.tagNames]
+	 * @param {integer} [msg.image.maxWidth]
+	 * @param {integer} [msg.image.maxHeight]
+	 * @param {array} [msg.image.tagNames]
 	 *		Use images with these tagNames.
 	 *		Array of strings.
-	 *
-	 * @return {Promise}
 	 */
-	module.exports = function docxWorker(options) {
-		return new Promise((resolve, reject) => {
-			if (!workerProcess) {
-				reject(new Error('DocxWorker process not available'));
-			}
-			else {
-				// Each job has a unique ID
-				options.jobID = workerJobCount;
-				workerJobCount += 1;
-				
-				workerProcess.send(options);
-				var messageHandler = function(msg) {
-					if (msg.jobID == options.jobID) {
-						workerProcess.removeListener('message', messageHandler);
-						if (msg.error) {
-							reject(msg.error);
-						}
-						else {
-							resolve(msg.result);
-						}
-					}
-				};
-				workerProcess.on('message', messageHandler);
-			}
-		});
-	};
-}
-
-
-// For fork()
-else {
-	var DocxGen = require('docxtemplater');
-	var DocxImageModule = require('docxtemplater-image-module');
-	var sizeOf = require('image-size');
-	var fs = require('fs');
-	var path = require('path');
-	var async = require('async');
-	
 	process.on('message', (msg) => {
 		if (msg && msg.data && msg.templateFile) {
 			var output = null;
@@ -149,18 +98,8 @@ else {
 			
 			], (err) => {
 				// Send results back to parent process
-				if (err) {
-					process.send({
-						jobID: msg.jobID,
-						error: err,
-					});
-				}
-				else {
-					process.send({
-						jobID: msg.jobID,
-						result: output,
-					});
-				}
+				if (err) process.send({ 'error': err });
+				else process.send(output);
 			});
 		}
 	});
